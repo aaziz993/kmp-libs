db92c18eab66188a9ae8e2effb057a11a4358ee0
Atoev Aziz <a.atoev93@gmail.com>
2024-04-24 14:42:19 +0500

    Initial Commit

--- .space.kts
+++ .space.kts
@@ -58,7 +58,7 @@ job("Code format, analysis and publish") {
         "Gradle test, build and publish to Maven Central",
         "aaziz93.registry.jetbrains.space/p/aaziz-93/containers/env-os:latest",
     ) {
-        env["SONATYPE_USER"] = "{{ project:sonatype_user }}"
+        env["SONATYPE_USER"] = "{{ project:sonatype_username }}"
         env["SONATYPE_PASSWORD"] = "{{ project:sonatype_password }}"
         env["SIGNING_GNUPG_PASSPHRASE"] = "{{ project:signing_gnupg_passphrase }}"
         shellScript {
