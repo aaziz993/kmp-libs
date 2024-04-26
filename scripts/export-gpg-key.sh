d18962030ed4d94df03a203ef474c5cd8be1041c
Atoev Aziz <a.atoev93@gmail.com>
2024-04-26 22:14:38 +0500

    Initial Commit

--- scripts/export-gpg-key.sh
+++ scripts/export-gpg-key.sh
@@ -16,23 +16,10 @@ if [[ -n "$SINGING_GNUPG_KEY_ID" && -n "$SINGING_GNUPG_KEY" ]];then
     ORG_GRADLE_PROJECT_signingInMemoryKeyId=SINGING_GNUPG_KEY_ID
     ORG_GRADLE_PROJECT_signingInMemoryKey=SINGING_GNUPG_KEY
 else
-
-    signing_gnupg_key_file="${SIGNING_GNUPG_FILE:=$(property "signing.gnupg.key.file" "$gradle_properties_file")}"
-    # GPG VARIABLES PROVIDED IN KEY FILE
-    if [[ -n "$signing_gnupg_key_file" ]];then
-        echo GET IN FILE GPG KEY
-            if [[ -f "$signing_gnupg_key_file" ]]; then
-                ORG_GRADLE_PROJECT_signingInMemoryKeyId="$(gpg --list-keys --keyid-format short "$(property "signing.gnupg.name.real" "$gradle_properties_file")" "$(property "signing.gnupg.key.file" "$gradle_properties_file")" | awk '$1 == "pub" { print $2 }' | cut -d'/' -f2)"
-                ORG_GRADLE_PROJECT_signingInMemoryKey="$(gpg --pinentry-mode=loopback --passphrase="$ORG_GRADLE_PROJECT_signingInMemoryKeyPassword" --export-secret-keys --armor "$ORG_GRADLE_PROJECT_signingInMemoryKeyId" "$(property "signing.gnupg.key.file" "$gradle_properties_file")" | grep -v '\-\-' | grep -v '^=.' | tr -d '\n')"
-            fi
-    # GPG VARIABLES PROVIDED IN MEMORY
-    else
-        echo GET IN MEMORY GPG KEY
-        if [[ -z "$(gpg --list-keys)" ]]; then
-            . gen-gpg-key.sh "$ORG_GRADLE_PROJECT_signingInMemoryKeyPassword"
-        fi
-        ORG_GRADLE_PROJECT_signingInMemoryKeyId="$(gpg --list-keys --keyid-format short "$(property "signing.gnupg.name.real" "$gradle_properties_file" )" | awk '$1 == "pub" { print $2 }' | cut -d'/' -f2)"
-        ORG_GRADLE_PROJECT_signingInMemoryKey="$(gpg --pinentry-mode=loopback --passphrase="$ORG_GRADLE_PROJECT_signingInMemoryKeyPassword" --export-secret-keys --armor "$ORG_GRADLE_PROJECT_signingInMemoryKeyId" | grep -v '\-\-' | grep -v '^=.' | tr -d '\n')"
+    if [[ -z "$(gpg --list-keys)" ]]; then
+        ./gen-gpg-key.sh "$ORG_GRADLE_PROJECT_signingInMemoryKeyPassword"
     fi
+    ORG_GRADLE_PROJECT_signingInMemoryKeyId="$(gpg --list-keys --keyid-format short "$(property "signing.gnupg.name.real" "$gradle_properties_file" )" | awk '$1 == "pub" { print $2 }' | cut -d'/' -f2)"
+    ORG_GRADLE_PROJECT_signingInMemoryKey="$(gpg --pinentry-mode=loopback --passphrase="$ORG_GRADLE_PROJECT_signingInMemoryKeyPassword" --export-secret-keys --armor "$ORG_GRADLE_PROJECT_signingInMemoryKeyId" | grep -v '\-\-' | grep -v '^=.' | tr -d '\n')"
 fi
 
